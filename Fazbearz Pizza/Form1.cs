e8d4e80c4d2e10809141bf95e7bce5f2bc8996b8
Christian Crank <ccrank2@students.kennesaw.edu>
2022-11-15 11:49:09 -0500

    Finished Person class. Didnt add ReceiptInfo...wasnt in class diagram

--- Fazbearz Pizza/Form1.cs
+++ Fazbearz Pizza/Form1.cs
@@ -6,5 +6,10 @@ namespace Fazbearz_Pizza
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
---
+++ Fazbearz Pizza/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Class1
+{
+	private string username;
+	private string password;
+
+
+	public Class1()
+	{
+	}
+
+	public string Username()
+	{
+		get { return username; }
+		set { return username; }
+	}
+
+	public string Password()
+	{
+		get { return password; }
+		set { return password; }
+	}
+
+	public bool Login(string str)
+	{
+		if (str.Equals(password))
+		{
+			return true;
+		}
+		else
+			return false;
+	}
+}
