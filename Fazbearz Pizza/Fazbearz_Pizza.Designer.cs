2aed5b99f21b1fd2ace4da197e9d3051275e96d1
xMEE <58533413+ZTexX@users.noreply.github.com>
2022-11-28 18:40:55 -0500

    Revamp

---
+++ Fazbearz Pizza/Fazbearz_Pizza.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Fazbearz_Pizza
+{
+    partial class Fazbearz_Pizza
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Fazbearz_Pizza));
+            this.MainMenu = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // MainMenu
+            // 
+            this.MainMenu.BackColor = System.Drawing.Color.White;
+            this.MainMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainMenu.Location = new System.Drawing.Point(0, 0);
+            this.MainMenu.Name = "MainMenu";
+            this.MainMenu.Size = new System.Drawing.Size(1902, 1033);
+            this.MainMenu.TabIndex = 0;
+            // 
+            // Fazbearz_Pizza
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.Controls.Add(this.MainMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Fazbearz_Pizza";
+            this.Text = "Fazbearz_Pizza";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Fazbearz_Pizza_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel MainMenu;
+    }
+}
---
+++ Fazbearz Pizza/Fazbearz_Pizza.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fazbearz_Pizza
+{
+    public partial class Fazbearz_Pizza : Form
+    {
+        private int originalFormWidth, originalFormHeight;
+
+        public Fazbearz_Pizza()
+        {
+            InitializeComponent();
+        }
+
+        private void Fazbearz_Pizza_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
